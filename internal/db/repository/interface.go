982e0e492eb3bc5ece039304efeb32c4f48780a4
Veli Ulugut <veliulugut1@gmail.com>
2024-08-13 20:55:26 +0300

    writing interface

--- internal/db/repository/interface.go
+++ internal/db/repository/interface.go
@@ -1,7 +1,14 @@
 package repository
 
-import "context"
+import (
+	"context"
+	"ticket-booking-app/internal/db/models"
+)
 
 type EventRepository interface {
-	GetMany(ctx context.Context)
+	GetMany(ctx context.Context) ([]*models.Event, error)
+	GetByID(ctx context.Context, id string) (*models.Event, error)
+	CreateEvent(ctx context.Context, event *models.Event) (*models.Event, error)
+	UpdateEvent(ctx context.Context, eventID uint, updateData map[string]interface{}) (*models.Event, error)
+	DeleteEvent(ctx context.Context, eventID uint) error
 }
