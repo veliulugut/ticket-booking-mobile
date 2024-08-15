321b09e612da13bc36e63ec508a57bc43e262500
Veli Ulugut <veliulugut1@gmail.com>
2024-08-15 15:40:37 +0300

    init func complete

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,122 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"database/sql"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"gorm.io/gorm"
+	"os"
+	"os/signal"
+	"sync"
+	"ticket-booking-app/cmd/api"
+	"ticket-booking-app/cmd/config"
+	"ticket-booking-app/internal/db/connection"
+	"ticket-booking-app/internal/i18n"
+	"time"
+)
+
+var (
+	once      sync.Once
+	conn      *gorm.DB
+	serveConf config.ServeConfig
+)
+
+func init() {
+	once.Do(func() {
+		conn = connection.PostgresSQLConnection(connection.DatabaseConfig{
+			Host:     os.Getenv("DB_HOST"),
+			Username: os.Getenv("DB_USER"),
+			Password: os.Getenv("DB_PASSWORD"),
+			DBName:   os.Getenv("DB_NAME"),
+			Port:     os.Getenv("DB_PORT"),
+			AppName:  os.Getenv("APP_NAME"),
+			SSLMode:  os.Getenv("DB_SSL_MODE"),
+			Timezone: os.Getenv("DB_TIMEZONE"),
+		})
+
+	})
+
+	serveConf = config.ServeConfig{
+		Host: os.Getenv("APP_HOST"),
+		Port: os.Getenv("APP_PORT"),
+	}
+
+	i18n.InitBundle("./internal/i18n/languages")
+}
 
 func main() {
-	fmt.Println("test")
+
+	app := fiber.New(config.FiberConfig)
+
+	app.Use(cors.New())
+	app.Use(recover.New())
+	app.Use(compress.New())
+	app.Use(logger.New(logger.Config{
+		TimeFormat: "2006-01-02T15:04:05.000Z",
+		TimeZone:   "Europe/Istanbul",
+	}))
+
+	api.InitializeRouter(app, conn)
+
+	// Start listening on port 8000
+	go func() {
+		if err := app.Listen(":8000"); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Graceful shutdown
+	err := GracefulShutdown(app, 5*time.Second)
+	if err != nil {
+		log.Error("Graceful shutdown error", err)
+	}
+}
+
+func GracefulShutdown(app *fiber.App, timeout time.Duration) error {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, os.Kill)
+
+	sig := <-sigChan
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	db, err := conn.DB()
+	if err != nil {
+		return err
+	}
+
+	if err := shutdownDatabase(ctx, db); err != nil {
+		return err
+	}
+
+	if err := app.Shutdown(); err != nil {
+		return err
+	}
+
+	log.Infof("Signal received: %v", sig)
+	return nil
+}
+
+func shutdownDatabase(ctx context.Context, db *sql.DB) error {
+	ch := make(chan error, 1)
+	go func() {
+		ch <- db.Close()
+	}()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case err := <-ch:
+		if err != nil {
+			log.Error("Database close error", err)
+			return err
+		}
+		return nil
+	}
 }
